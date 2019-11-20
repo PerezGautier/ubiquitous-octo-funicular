bdb9f8e6bfa84355c9db79330759241756e65571
pedago <pedago@pcalb-mm0413.univ-jfc.fr>
2019-11-20 14:33:43 +0100

    fichier de base avec exemples

---
+++ mini-projet/src/main/java/model/DAO.java
@@ -0,0 +1,25 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+import javax.sql.DataSource;
+
+
+
+public class DAO {
+
+	private final DataSource myDataSource;
+
+	/**
+	 * Construit le AO avec sa source de données
+	 * @param dataSource la source de données à utiliser
+	 */
+	public DAO(DataSource dataSource) {
+		this.myDataSource = dataSource;
+	}
+
+}
