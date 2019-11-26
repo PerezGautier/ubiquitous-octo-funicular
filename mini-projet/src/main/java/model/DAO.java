af489a5b1a8c203977b8231e4e9420af6500eb8c
pedago <pedago@pcalb-mm0615.univ-jfc.fr>
2019-11-26 15:50:07 +0100

    Première requête du DAO

--- mini-projet/src/main/java/model/DAO.java
+++ mini-projet/src/main/java/model/DAO.java
@@ -11,15 +11,34 @@ import javax.sql.DataSource;
 
 
 public class DAO {
+    private final DataSource myDataSource;
 
-	private final DataSource myDataSource;
+    /**
+     * Construit le DAO avec sa source de données
+     * @param dataSource la source de données à utiliser
+     */
+    public DAO(DataSource dataSource) {
+            this.myDataSource = dataSource;
+    }
 
-	/**
-	 * Construit le AO avec sa source de données
-	 * @param dataSource la source de données à utiliser
-	 */
-	public DAO(DataSource dataSource) {
-		this.myDataSource = dataSource;
+    /**
+     * @return Liste de toutes les catégories
+     * @throws SQLException renvoyées par JDBC
+     */
+    public List<String> toutesCategories() throws SQLException {
+
+	List<String> result = new LinkedList<>();
+
+	String sql = "SELECT Libelle FROM Categorie";
+	try (Connection connection = myDataSource.getConnection(); 
+		 PreparedStatement stmt = connection.prepareStatement(sql)) {
+		ResultSet rs = stmt.executeQuery();
+		while (rs.next()) {
+			String libelle = rs.getString("Libelle");
+			result.add(libelle);
+		}
 	}
+	return result;
+    }
 
 }
